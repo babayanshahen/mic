946e27f5b53d2dffcc6a4751089e759a23747905
babayanshahen <shahenbabayan@gmail.com>
2017-10-12 04:03:19 +0400

    zangvacner

---
+++ Zangvacner/Zangvacner/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zangvacner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter array size");
+            int sizeOfArray = int.Parse(Console.ReadLine());
+            //Console.WriteLine(n);
+            Random rd = new Random();
+            // double number = rd.NextDouble();
+            //Console.WriteLine(number);
+            //(maximum - minimum) + minimum;
+            double[] doubleArray = new double[sizeOfArray];
+            Console.WriteLine("arajadranq 1.a");
+            for (int i = 0; i < sizeOfArray; i++)
+            {
+                double number = rd.NextDouble() * 100;
+                if (number <100 && number >10 && number != 0)
+                {
+                    doubleArray[i] = number;
+                    
+                }
+                Console.WriteLine(doubleArray[i]);
+
+            }
+            Console.WriteLine("arajadranq 1.b");
+            bool isOdd = false;
+            foreach (var doubleA in doubleArray)
+            {
+                if (isOdd)
+                {
+                    Console.WriteLine(doubleA);
+                    isOdd = false;
+                }
+                else
+                {
+                    isOdd = true;
+                }
+                    
+            }
+            Console.WriteLine("arajadranq 1.c");
+            foreach (var doubleA in doubleArray)
+            {
+                if (Math.Truncate(doubleA
+                    ) % 2 == 0)
+                {
+                    Console.WriteLine(doubleA);
+                }
+            }
+
+            Console.WriteLine("arajadranq 1.d");
+            Console.WriteLine(doubleArray.Min());
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ miclesson1/miclesson1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace miclesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nshel erkarutyun: ");
+            int erkarutyun = int.Parse(Console.ReadLine());
+            Console.Write("Nshel laynutyun: ");
+            int laynutyun = int.Parse(Console.ReadLine());
+            int Paragic = (erkarutyun + laynutyun)*2;
+            int Makeres = erkarutyun * laynutyun;
+            string result = "Paragicy:"+ Paragic+"  Makeresy: "+Makeres;
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            Console.SetCursorPosition((Console.WindowWidth - result.Length) / 2, Console.CursorTop);
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Black;
+            for (int i = 0; i < erkarutyun; i++ )
+            {
+                if (i == 0 || i < (laynutyun))
+                {
+                    for (int j = 0; j < erkarutyun; j++ )
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            //string zig = "you Can do  this application in  two  days";
+            //char[] chArray = zig.ToCharArray();
+
+            //Array.Reverse(chArray);
+
+            //foreach (char zigChar in chArray)
+            //    Console.Write(zigChar);
+            //Console.ReadLine();
+            Console.Write("Nshel zangvaci chapy: ");
+            int lenghtOfArray = int.Parse(Console.ReadLine());
+            int[] makedArray = new int[(lenghtOfArray+1)];
+            Random rnd = new Random();
+            //Console.WriteLine(rnd.Next(-99,100));
+
+            for (int i = 0; i <  lenghtOfArray ; i++)
+            {
+
+                makedArray[i] = rnd.Next(-99,100);
+                Console.WriteLine("index - "+i+" "+ "Arjeq - "+ makedArray[i]);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
